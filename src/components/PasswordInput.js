1ecff866680cc661ba3c33644b07856ae68fbafd
nadinho <rockt2935@gmail.com>
2020-03-31 22:59:14 +0200

    Change background color on input

--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -8,12 +8,23 @@ const Input = styled.input`
   font-size: 2.2rem;
   padding: 8px;
   &:focus {
-    background-color: red;
+    background-color: hsl(${props => props.bgColor}, 100%, 50%);
   }
 `;
 
 function PasswordInput({ value }) {
-  return <Input type="password" value={value}></Input>;
+  const setValue = React.useState("");
+  function changeBgColor() {
+    return Math.min(value.length * 10, 120);
+  }
+  return (
+    <Input
+      type="password"
+      value={value}
+      onChange={event => setValue(event.target.value)}
+      bgColor={changeBgColor}
+    ></Input>
+  );
 }
 
 export default PasswordInput;
