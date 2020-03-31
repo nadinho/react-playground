b096a83e4bea05fec5585015f7c0925243025972
nadinho <rockt2935@gmail.com>
2020-03-31 22:57:09 +0200

    Style PasswordInput

--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Input = styled.input`
+  height: 45px;
+  width: 250px;
+  outline: 2px solid black;
+  font-size: 2.2rem;
+  padding: 8px;
+  &:focus {
+    background-color: red;
+  }
+`;
 
 function PasswordInput({ value }) {
-  return <input type="password" value={value} />;
+  return <Input type="password" value={value}></Input>;
 }
 
 export default PasswordInput;
